2c76a6e0bfcd3ccf8eb405be0d3563d8ecc2e7f0
lctrevizan <l@lct.cx>
2023-01-03 00:26:34 -0300

    Removed some .mk files for testing

--- twrp_xpeng.mk
+++ twrp_xpeng.mk
@@ -5,11 +5,8 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/twrp/config/common_full_phone.mk)
 
 # Inherit from xpeng device
 $(call inherit-product, device/motorola/xpeng/device.mk)
